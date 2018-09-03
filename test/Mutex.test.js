5677fbf32588e6ea99b6ca4c1dff6be877fda536
plasma.wu <plasma.wu@104.com.tw>
2018-09-03 16:31:15 +0800

    調整 Mutex和Semaphore Test 的 架構

--- test/Mutex.test.js
+++ test/Mutex.test.js
@@ -9,136 +9,158 @@ const api = supertest('http://localhost:8080/v1/mutex')
 
 let handle;
 let expiry;
+const testkey = uuid.v4()
 
-before(function (done) {
-    require('../index.js')
-    done()
-});
+describe('Test Mutex Api', function () {
 
-describe('Lock Mutex', function () {
-    it('lock mutex', (done) => {
-        api.put('/testkey').send({
-            'ttl': 1
-        }).expect(200).end((err, res) => {
+    before('Test Environment', done => {
+        api.get(`/${testkey}notExistKey`).expect(404).end((err, res) => {
             if (err) {
                 done(err);
                 return;
             }
-            expect(res.body).to.have.property('handle');
-            expect(res.body).to.have.property('id');
-            expect(res.body).to.have.property('locked');
-            expect(res.body).to.have.property('expiry');
-            handle = res.body.handle
-            expiry = res.body.expiry
+            expect(res.body).to.have.property('message');
+            assert(res.body.message === 'MutextKey Or Handler Not Exist');
             done();
         })
     })
 
-    it('lock mutex again until expiry', (done) => {
-        setTimeout(function () {
-            api.put('/testkey').expect(200).end((err, res) => {
+    describe('Lock Mutex', function () {
+        it('lock mutex', (done) => {
+            api.post(`/${testkey}`).send({
+                'ttl': 1
+            }).expect(200).end((err, res) => {
                 if (err) {
                     done(err);
                     return;
                 }
-                assert(res.body.handle !== handle);
+                expect(res.body).to.have.property('handle');
+                expect(res.body).to.have.property('id');
+                expect(res.body).to.have.property('expiry');
                 handle = res.body.handle
                 expiry = res.body.expiry
                 done();
             })
-        }, expiry * 1000 - Date.now())
-    })
+        })
 
-    it('lock mutex again immediately', (done) => {
-        api.put('/testkey').expect(409).end((err, res) => {
-            if (err) {
-                done(err);
-                return;
-            }
-            expect(res.body).to.have.property('message');
-            assert(res.body.message === 'lock in use');
-            done();
+        it('lock mutex again until expiry', (done) => {
+            setTimeout(function () {
+                api.post(`/${testkey}`).expect(200).end((err, res) => {
+                    if (err) {
+                        done(err);
+                        return;
+                    }
+                    assert(res.body.handle !== handle);
+                    handle = res.body.handle
+                    expiry = res.body.expiry
+                    done();
+                })
+            }, expiry * 1000 - Date.now())
+        })
+
+        it('lock mutex again immediately', (done) => {
+            api.post(`/${testkey}`).expect(409).end((err, res) => {
+                if (err) {
+                    done(err);
+                    return;
+                }
+                expect(res.body).to.have.property('message');
+                assert(res.body.message === 'Lock In Use');
+                done();
+            })
+        })
+    });
+
+
+    describe('Query Mutex', function () {
+        it('query mutex status', (done) => {
+            api.get(`/${testkey}`).expect(200).end((err, res) => {
+                if (err) {
+                    done(err);
+                    return;
+                }
+                expect(res.body).to.have.property('id');
+                expect(res.body).to.have.property('expiry');
+                done();
+            })
+        })
+
+        it('query not exist mutex', (done) => {
+            api.get("/" + uuid.v4()).expect(404).end((err, res) => {
+                if (err) {
+                    done(err);
+                    return;
+                }
+                expect(res.body).to.have.property('message');
+                assert(res.body.message === 'MutextKey Or Handler Not Exist');
+                done();
+            })
+        })
+    });
+
+    describe('Update Mutex TTL', function () {
+        it('update ttl', done => {
+            api.patch(`/${testkey}/` + handle).send({
+                'ttl': 1000
+            }).expect(200).end((err, res) => {
+                if (err) {
+                    done(err);
+                    return;
+                }
+                expect(res.body).to.have.property('handle');
+                expect(res.body).to.have.property('id');
+                expect(res.body).to.have.property('expiry');
+                assert(res.body.expiry === expiry + 1000);
+                done();
+            })
+        })
+
+        it('update ttl no ttl body', done => {
+            api.patch(`/${testkey}/` + handle).expect(400).end((err, res) => {
+                if (err) {
+                    done(err);
+                    return;
+                }
+                expect(res.body).to.have.property('message');
+                assert(res.body.message === 'TTL Format Error Or Not Exist');
+                done();
+            })
+        })
+
+        it('update ttl when handler error', done => {
+            api.patch(`/${testkey}/` + 'errorhandler').send({
+                'ttl': 1000
+            }).expect(404).end((err, res) => {
+                if (err) {
+                    done(err);
+                    return;
+                }
+                expect(res.body).to.have.property('message');
+                assert(res.body.message === 'MutextKey Or Handler Not Exist');
+                done();
+            })
         })
     })
-});
 
-
-describe('Query Mutex', function () {
-    it('query mutex status', (done) => {
-        api.get('/testkey').expect(200).end((err, res) => {
-            if (err) {
-                done(err);
-                return;
-            }
-            expect(res.body).to.not.have.property('handle');
-            expect(res.body).to.have.property('id');
-            expect(res.body).to.have.property('locked');
-            expect(res.body).to.have.property('expiry');
-            done();
+    describe('Delete Mutex', function () {
+        it('delete mutex', done => {
+            api.delete(`/${testkey}/` + handle).expect(200).end((err, res) => {
+                if (err) {
+                    done(err);
+                    return;
+                }
+                done();
+            })
         })
-    })
 
-    it('query not exist mutex', (done) => {
-        api.get("/" + uuid.v4()).expect(400).end((err, res) => {
-            if (err) {
-                done(err);
-                return;
-            }
-            done();
-        })
-    })
-});
-
-describe('Update Mutex TTL', function () {
-    it('update ttl', done => {
-        api.patch('/testkey/' + handle).send({
-            'ttl': 1000
-        }).expect(200).end((err, res) => {
-            if (err) {
-                done(err);
-                return;
-            }
-            expect(res.body).to.have.property('handle');
-            expect(res.body).to.have.property('id');
-            expect(res.body).to.have.property('locked');
-            expect(res.body).to.have.property('expiry');
-            assert(res.body.expiry === expiry + 1000);
-            done();
-        })
-    })
-
-    it('update ttl no ttl body', done => {
-        api.patch('/testkey/' + handle).expect(400).end((err, res) => {
-            if (err) {
-                done(err);
-                return;
-            }
-            console.log(res.body)
-            done();
-        })
-    })
-})
-
-describe('Delete Mutex', function () {
-    it('delete mutex', done => {
-        api.delete('/testkey/' + handle).expect(200).end((err, res) => {
-            if (err) {
-                done(err);
-                return;
-            }
-            done();
-        })
-    })
-
-    it('delete not exist mutex', done => {
-        api.delete('/testkey/' + handle).expect(400).end((err, res) => {
-            if (err) {
-                done(err);
-                return;
-            }
-            expect(res.body).to.have.property('message');
-            assert(res.body.message === 'Invalid ID Supplied');
-            done();
+        it('delete not exist mutex', done => {
+            api.delete(`/${testkey}/` + handle).expect(200).end((err, res) => {
+                if (err) {
+                    done(err);
+                    return;
+                }
+                done();
+            })
         })
     })
 })
--- test/Semaphore.test.js
+++ test/Semaphore.test.js
@@ -6,39 +6,13 @@ const {
 } = require('chai')
 
 const api = supertest('http://localhost:8080/v1/semaphore')
-
-let handle;
+const seat = 1
 let testkey = uuid.v4()
 
-// before(function (done) {
-//     require('../index.js')
-//     done()
-// });
+describe('Test Semaphore Api', () => {
 
-describe('Create Semaphore', function () {
-    it('create semaphore', done => {
-        api.put(`/${testkey}`).send({
-            'capacity': 2
-        }).expect(200).end(((err, res) => {
-            if (err) {
-                done(err);
-                return;
-            }
-            expect(res.body).to.have.property('handle');
-            handle = res.body.handle
-            expect(res.body).to.have.property('id');
-            expect(res.body).to.have.property('count');
-            expect(res.body).to.have.property('capacity');
-            assert(res.body.capacity === 2);
-            expect(res.body).to.have.property('expiry');
-            done();
-        }))
-    })
-
-    it('create semaphore again', done => {
-        api.put(`/${testkey}`).send({
-            'capacity': 5
-        }).expect(409).end((err, res) => {
+    before('Test Environment', done => {
+        api.get(`/${testkey}notExistKey`).expect(404).end((err, res) => {
             if (err) {
                 done(err);
                 return;
@@ -46,116 +20,209 @@ describe('Create Semaphore', function () {
             done();
         })
     })
-})
 
-describe('Get Semaphore Status', function () {
-    it('get semaphore status', done => {
-        api.get(`/${testkey}/${handle}`).expect(200).end((err, res) => {
-            if (err) {
-                done(err);
-                return;
-            }
-            expect(res.body).to.have.property('handle');
-            expect(res.body).to.have.property('id');
-            expect(res.body).to.have.property('count');
-            expect(res.body).to.have.property('capacity');
-            expect(res.body).to.have.property('expiry');
-            done();
-        })
-    })
-})
+    describe('Create Semaphore', () => {
 
-describe('Acquires A Permit', function () {
-    // before(function (done) {
-    //     done()
-    // });
-
-    it('Acquires a permit from semaphore', done => {
-        api.patch(`/acquires/${testkey}/${handle}`).expect(200).end((err, res) => {
-            if (err) {
-                done(err);
-                return;
-            }
-            expect(res.body).to.have.property('handle');
-            expect(res.body).to.have.property('id');
-            expect(res.body).to.have.property('count');
-            assert(res.body.count === 1);
-            expect(res.body).to.have.property('capacity');
-            expect(res.body).to.have.property('expiry');
+        it('create semaphore', done => {
+            api.post(`/${testkey}`).send({
+                'seat': seat
+            }).expect(200).end((err, res) => {
+                if (err) {
+                    done(err);
+                    return;
+                }
+                expect(res.body).to.have.property('id');
+                expect(res.body).to.have.property('seat');
+                assert(res.body.seat === seat)
+                assert(res.body.id === testkey)
+                done()
+            })
         })
 
-        api.patch(`/acquires/${testkey}/${handle}`).expect(200).end((err, res) => {
-            if (err) {
-                done(err);
-                return;
-            }
-            assert(res.body.count === 2);
-            done()
+        it('create Semaphore again with same key', done => {
+            api.post(`/${testkey}`).send({
+                'seat': seat
+            }).expect(409).end((err, res) => {
+                if (err) {
+                    done(err);
+                    return;
+                }
+                expect(res.body).to.have.property('message');
+                assert(res.body.message === 'Lock In Use')
+                done()
+            })
         })
     })
 
-    it('Acquires a permit exceed capacity', done => {
-        api.patch(`/acquires/${testkey}/${handle}`).expect(400).end((err, res) => {
-            if (err) {
-                done(err);
-                return;
-            }
+    describe('Get Semaphore Status', function () {
+
+        it('get semaphore status', done => {
+            api.get(`/${testkey}`).expect(200).end((err, res) => {
+                if (err) {
+                    done(err);
+                    return;
+                }
+                expect(res.body).to.have.property('id');
+                expect(res.body).to.have.property('seat');
+                expect(res.body).to.have.property('occupied');
+                assert(res.body.seat === seat)
+                assert(res.body.occupied === 0)
+                done();
+            })
         })
 
-        api.get(`/${testkey}/${handle}`).expect(200).end((err, res) => {
-            if (err) {
-                done(err);
-                return;
-            }
-            assert(res.body.count === 2);
-            done()
-        })
-    })
-})
-
-
-describe('Releases A Permit', function () {
-    it('Releases a permit from semaphore', done => {
-        api.patch(`/releases/${testkey}/${handle}`).expect(200).end((err, res) => {
-            if (err) {
-                done(err);
-                return;
-            }
-            assert(res.body.count === 1);
-            done()
+        it('get semaphore status with not existing key', done => {
+            api.get(`/${testkey}notexistingkey`).expect(404).end((err, res) => {
+                if (err) {
+                    done(err);
+                    return;
+                }
+                expect(res.body).to.have.property('message');
+                assert(res.body.message === 'Key Not Exist')
+                done();
+            })
         })
     })
 
-    it('Releases a permit from semaphore', done => {
-        api.patch(`/releases/${testkey}/${handle}`).expect(200).end((err, res) => {
-            if (err) {
-                done(err);
-                return;
-            }
-            assert(res.body.count === 0);
-            done()
+    describe('Acquires a seat', function () {
+        let handler;
+        let expiry;
+
+        it('Acquires a seat with not exist semaphore', done => {
+            api.post(`/${testkey}notExisting/seat`).expect(404).end((err, res) => {
+                if (err) {
+                    done(err);
+                    return;
+                }
+                expect(res.body).to.have.property('message');
+                assert(res.body.message === 'Key Not Exist')
+                done()
+            })
+        })
+
+        it('Acquires a seat from semaphore', done => {
+            api.post(`/${testkey}/seat`).send({
+                ttl: 1
+            }).expect(200).end((err, res) => {
+                if (err) {
+                    done(err);
+                    return;
+                }
+                expect(res.body).to.have.property('id');
+                expect(res.body).to.have.property('handler');
+                expect(res.body).to.have.property('expiry');
+                expiry = res.body.expiry
+                done()
+            })
+        })
+
+        it('Acquires a seat again until expiry', done => {
+            setTimeout(() => {
+                api.post(`/${testkey}/seat`).expect(200).end((err, res) => {
+                    if (err) {
+                        done(err);
+                        return;
+                    }
+                    handler = res.body.handler
+                    done()
+                })
+            }, expiry * 1000 - Date.now())
+        })
+
+        it('Acquires a seat again immediately', done => {
+            api.post(`/${testkey}/seat`).expect(409).end((err, res) => {
+                if (err) {
+                    done(err);
+                    return;
+                }
+                expect(res.body).to.have.property('message');
+                assert(res.body.message === 'No Seat Available')
+                done()
+            })
+        })
+
+        it('Releases a seat from semaphore', done => {
+            api.delete(`/${testkey}/seat/${handler}`).expect(200).end((err, res) => {
+                if (err) {
+                    done(err);
+                    return;
+                }
+                done()
+            })
+        })
+
+        it('Releases a seat with not exist handler', done => {
+            api.delete(`/${testkey}/seat/${handler}notexist`).expect(200).end((err, res) => {
+                if (err) {
+                    done(err);
+                    return;
+                }
+                done()
+            })
         })
     })
 
-    it('Releases a permit from semaphore', done => {
-        api.patch(`/releases/${testkey}/${handle}`).expect(400)
-        done()
-    })
-})
+    describe('Delete semaphore', function () {
 
-describe('Delete Semaphore', _ => {
-    it('delete semaphore', done => {
-        api.delete(`/${testkey}/${handle}`).expect(200).end((err, res) => {
-            if (err) {
-                done(err);
-                return;
-            }    
+        it('delete not exist semaphore', done => {
+            api.delete(`/${testkey}noExist`).expect(200).end((err, res) => {
+                if (err) {
+                    done(err)
+                    return
+                }
+                done()
+            })
         })
-        done()
-    })
 
-    it('delete not exist semaphore', done => {
-        api.delete(`/${testkey}/${handle}`).expect(400)
-        done()
+        it('delete semaphore when someone occupy a seat', done => {
+            api.post(`/${testkey}/seat`).send({
+                ttl: 60
+            }).expect(200).end((err, res) => {
+                if (err) {
+                    done(err)
+                    return
+                }
+                const {
+                    handler
+                } = res.body
+                api.delete(`/${testkey}`).expect(409).end((err, res) => {
+                    if (err) {
+                        done(err)
+                        return
+                    }
+                    expect(res.body).to.have.property('message');
+                    assert(res.body.message === 'Lock In Use')
+
+                    api.delete(`/${testkey}/seat/${handler}`).expect(200).end((err, res) => {
+                        if (err) {
+                            throw err
+                        }
+                        done()
+                    })
+                })
+            })
+        })
+
+        it('delete semaphore when seat expired', done => {
+            api.post(`/${testkey}/seat`).send({
+                ttl: 1
+            }).expect(200).end((err, res) => {
+                if (err) {
+                    done(err)
+                    return
+                }
+                const {
+                    expiry
+                } = res.body
+                setTimeout(function () {
+                    api.delete(`/${testkey}`).expect(200).end((err, res) => {
+                        if (err) {
+                            throw err
+                        }
+                        done()
+                    })
+                }, expiry * 1000 - Date.now())
+            })
+        })
     })
 })
